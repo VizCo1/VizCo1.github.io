f68e387a96e79202aba81bab3eee3a9e9b6bb4ca
VizCoDev <62243456+VizCo1@users.noreply.github.com>
2025-03-15 23:43:39 +0100

    WIP

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,9 +1,16 @@
 const projectButton01 = document.getElementById("project-button-01");
 const projectButton02 = document.getElementById("project-button-02");
 const popupContainer = document.getElementById("video-popup-container");
+const popupCloseButton = document.getElementById("video-popup-close-button");
 
+popupCloseButton.onclick = function() { hideVideoPopup(); }
 projectButton01.onclick = function() {showVideoPopup();};
 
 function showVideoPopup() {
   popupContainer.style.display = "flex";
 };
+
+function hideVideoPopup() {
+  popupContainer.style.display = "none";
+};
+
