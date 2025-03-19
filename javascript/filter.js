c9f051f46a9b6668aabbf24568528d167ee00b71
VizCoDev <62243456+VizCo1@users.noreply.github.com>
2025-03-20 00:19:50 +0100

    WIP

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('.nav-main-item');
-    const items = document.querySelectorAll('.project');
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".nav-main-item");
+    const items = document.querySelectorAll(".project");
+
+    buttons[0].classList.add("active")
 
     buttons.forEach(button => {
-        button.addEventListener('click', function () {
-            const filterValue = this.getAttribute('data-filter');
+        button.addEventListener("click", function () {
+            const filterValue = this.getAttribute("data-filter");
 
             items.forEach(item => {
-                const itemFilter = item.getAttribute('data-filter');
-                if (filterValue === 'all' || filterValue === itemFilter) {
-                    item.style.display = 'initial';
+                const itemFilter = item.getAttribute("data-filter");
+                if (filterValue === "all" || filterValue === itemFilter) {
+                    item.style.display = "initial";
                 } else {
-                    item.style.display = 'none';
+                    item.style.display = "none";
                 }
             });
 
-            buttons.forEach(btn => btn.classList.remove('active'));
-            this.classList.add('active');
+            buttons.forEach(btn => btn.classList.remove("active"));
+            this.classList.add("active");
         });
     });
 });
